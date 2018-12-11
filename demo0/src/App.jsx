cc90a5a358a1d1b26d45f431a0a28c986f4f8952
goingMaze <823002822@qq.com>
2018-12-11 18:22:02 +0800

    Add: editorconfig

--- demo0/src/App.jsx
+++ demo0/src/App.jsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import Router from './router';
 
 class App extends Component {
-	render() {
-		// return React.createElement('h1', {}, '我就想看看webpack怎么零配置');
-		return (
-			<div>
-				<h1>我就想看看webpack怎么零配置</h1>
-				<Router />
-			</div>
-		);
-	}
+  render() {
+    // return React.createElement('h1', {}, '我就想看看webpack怎么零配置');
+    return (
+      <div>
+        <h1>我就想看看webpack怎么零配置</h1>
+        <Router />
+      </div>
+    );
+  }
 }
 
 export default App;
